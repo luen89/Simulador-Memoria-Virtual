a3c4a7e788cc3ececcf23580e3826a92a4dcb2e3
luen89 <60421972+luen89@users.noreply.github.com>
2020-01-29 01:17:33 -0600

    Metodo que devuelve el numero sel segmento

--- segmento.java
+++ segmento.java
@@ -1,9 +1,18 @@
-public class segmento{
-	public long inicio;
-	public long fin;	
+public class Segment{
+long dir_ini; // Se creo la clase segment
+long dir_fin;
+int index;
+Segment(long dir_ini, long dir_fin, int index){
+	this.dir_ini=dir_ini;
+	this.dir_fin=dir_fin;
+	this.index=index;
+}
 
-	public segmento(long inicio, long fin){
-		this.inicio=inicio;
-		this.fin=fin;
-	}
+int is_in(long diri,long dirf){
+	if(diri>=dir_ini && dirf<=dir_fin) // Verifica que la pagina este dentro del segmento si es asi retorna el valor del segmento caso contrario retorna -1
+		return index;
+	else
+		return -1;
+
+}
 }
